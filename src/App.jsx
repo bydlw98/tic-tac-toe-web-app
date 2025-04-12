6440683cb5116b251325b4258b614d26279d8df2
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2025-04-12 21:43:20 +0800

    feat: add support for taking turns between 'X' and 'O'

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
 import { useState } from "react";
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue("X");
-  }
+function Square({ value, onSquareClicked }) {
   return (
-    <button className="square" onClick={handleClick}>
+    <button className="square" onClick={onSquareClicked}>
       {value}
     </button>
   );
 }
 
 function App() {
+  const [xTurn, setXTurn] = useState(true);
+  const [squares, setSquaresValue] = useState(Array(9).fill(null));
+
+  function onSquareClicked(i) {
+    if (squares[i]) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+
+    if (xTurn) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    setSquaresValue(nextSquares);
+    setXTurn(!xTurn);
+  }
+
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClicked={() => onSquareClicked(0)} />
+        <Square value={squares[1]} onSquareClicked={() => onSquareClicked(1)} />
+        <Square value={squares[2]} onSquareClicked={() => onSquareClicked(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClicked={() => onSquareClicked(3)} />
+        <Square value={squares[4]} onSquareClicked={() => onSquareClicked(4)} />
+        <Square value={squares[5]} onSquareClicked={() => onSquareClicked(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClicked={() => onSquareClicked(6)} />
+        <Square value={squares[7]} onSquareClicked={() => onSquareClicked(7)} />
+        <Square value={squares[8]} onSquareClicked={() => onSquareClicked(8)} />
       </div>
     </>
   );
